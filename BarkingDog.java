6b9da469ea1271a959b244626e9844f2808ad5a7
Akshay Subhash Wagh <81297179+akshaywagh3@users.noreply.github.com>
2022-01-15 12:03:32 +0530

    Add files via upload

---
+++ BarkingDog.java
@@ -0,0 +1,25 @@
+public class BarkingDog
+{
+    public static void main(String[] args)
+    {
+        shouldWakeUp(true,10);
+        shouldWakeUp(false,23);
+
+
+    }
+    public static boolean shouldWakeUp(boolean barking,int hourOfDay)
+    {
+
+        if(hourOfDay<0 || hourOfDay>23){
+            return false;
+        }
+        if(barking && (hourOfDay<8 || hourOfDay>22 )){
+            return true;
+        }
+        return false;
+
+
+
+    }
+
+}
